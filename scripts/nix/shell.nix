4c296046b97fd9ac83c41274cf65ce03237a2931
Rae Knowler <bellisk@users.noreply.github.com>
2023-11-20 10:21:50 +0100

    Update shell.nix

--- scripts/nix/shell.nix
+++ scripts/nix/shell.nix
@@ -3,22 +3,6 @@ let
 in
 with nixpkgs;
 
-let dedup = stdenv.mkDerivation {
-    name = "dedup";
-    src = if system == "x86_64-darwin" then fetchurl {
-      url = "http://example.org/libfoo-1.2.3.tar.bz2";
-      sha256 = "0x2g1jqygyr5wiwg4ma1nd7w4ydpy82z9gkcv8vh2v8dn3y58v5m";
-    } else if system == "x86_64-windows" then fetchurl {
-      url = "http://example.org/libfoo-1.2.3.tar.bz2";
-      sha256 = "0x2g1jqygyr5wiwg4ma1nd7w4ydpy82z9gkcv8vh2v8dn3y58v5m";
-    } else fetchurl {
-      url = "http://example.org/libfoo-1.2.3.tar.bz2";
-      sha256 = "0x2g1jqygyr5wiwg4ma1nd7w4ydpy82z9gkcv8vh2v8dn3y58v5m";
-    }
-    ;
-  };
-in
-
 pkgs.mkShell {
   nativeBuildInputs = [ 
     
@@ -27,7 +11,6 @@ pkgs.mkShell {
     openssl
     curl
     git
-    dedup
   ];
 }
 
